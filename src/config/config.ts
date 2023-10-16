44a722a69252a89ede0f01ee8d0713d917767182
matiaspiris17 <matiaslinkedin17@gmail.com>
2023-10-16 20:02:57 -0300

    docker y variables de entorno

---
+++ src/config/config.ts
@@ -0,0 +1,31 @@
+import * as dotenv from "dotenv";
+
+export abstract class ConfigServer {
+  constructor() {
+    const nodeNameEnv = this.createPathEnv(this.nodeEnv);
+    dotenv.config({
+        path:nodeNameEnv
+    })
+  }
+
+  public getEnvironment(k: string): string | undefined {
+    return process.env[k]; // process.env.["PORT"]
+  }
+  public getNumberEnv(k: string): number {
+    return Number(this.getEnvironment(k));
+  }
+
+  public get nodeEnv(): string {
+    return this.getEnvironment("NODE_ENV")?.trim() || "";
+  }
+
+  public createPathEnv(path: string): string {
+    const arrEnv: Array<string> = ["env"]; // string[]
+
+    if (path.length > 0) {
+        const stringToArray = path.split(".")
+        arrEnv.unshift(...stringToArray)
+    }
+    return "." + arrEnv.join(".") ;
+  }
+}
