91124706ac0af93112e016fc901cd5b26164d6f5
matiaspiris17 <matiaslinkedin17@gmail.com>
2023-10-26 16:22:27 -0300

    manejo de errores user

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,15 +1,23 @@
 import { Request, Response } from "express";
 import { UserService } from "../services/user.service";
+import { HttpResponse } from "../../shared/router/response/http.response";
+import { DeleteResult, UpdateResult } from "typeorm";
 
 export class UserController {
-  constructor(private readonly userService: UserService = new UserService()) {}
+  constructor(
+    private readonly userService: UserService = new UserService(),
+    private readonly httpResponse: HttpResponse = new HttpResponse()
+  ) {}
 
   async getUsers(req: Request, res: Response) {
     try {
       const data = await this.userService.findAllUser();
-      res.status(200).json(data);
+      if (data.length === 0)
+        return this.httpResponse.NotFound(res, "No existe dato");
+      return this.httpResponse.OK(res, data);
     } catch (e) {
       console.error(e);
+      return this.httpResponse.Error(res, e);
     }
   }
 
@@ -17,37 +25,45 @@ export class UserController {
     const { id } = req.params;
     try {
       const data = await this.userService.findUserById(id);
-      res.status(200).json(data);
+      if(!data)
+        return this.httpResponse.NotFound(res, "No existe dato");
+      return this.httpResponse.OK(res, data);
     } catch (e) {
       console.error(e);
+      return this.httpResponse.Error(res, e);
     }
   }
 
   async createUser(req: Request, res: Response) {
     try {
       const data = await this.userService.createUser(req.body);
-      res.status(200).json(data);
+      return this.httpResponse.OK(res, data);
     } catch (e) {
       console.error(e);
+      return this.httpResponse.Error(res, e);
     }
   }
 
   async updateUser(req: Request, res: Response) {
-    const {id} = req.params
+    const { id } = req.params;
     try {
-      const data = await this.userService.updateUser(id, req.body);
-      res.status(200).json(data);
+      const data: UpdateResult = await this.userService.updateUser(id, req.body);
+      if(!data.affected) return this.httpResponse.NotFound(res, "Hay un error en actualizar"); 
+      return this.httpResponse.OK(res, data);
     } catch (e) {
       console.error(e);
+      return this.httpResponse.Error(res, e);
     }
   }
 
   async deleteUser(req: Request, res: Response) {
     try {
-      const data = await this.userService.deleteUser(req.params.id);
-      res.status(200).json(data);
+      const data: DeleteResult = await this.userService.deleteUser(req.params.id);
+      if(!data.affected) return this.httpResponse.NotFound(res, "Hay un error en actualizar"); 
+      return this.httpResponse.OK(res, data);
     } catch (e) {
       console.error(e);
+      return this.httpResponse.Error(res, e);
     }
-  }  
-}
+  }
+} 
